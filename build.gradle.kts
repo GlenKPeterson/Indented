8bedc288a2411c8016db84b225a3d57df87c63df
Glen K. Peterson <glen.k.peterson@gmail.com>
2020-04-01 19:58:18 -0400

    little build-file cleanup.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,9 @@ import org.jetbrains.dokka.gradle.DokkaTask
 // https://central.sonatype.org/pages/gradle.html
 
 // To upload to sonatype (have to deploy manually)
+// Now:
+// ./gradlew clean assemble publish --info
+// Used to be:
 // ./gradlew clean uploadArchives --info
 
 // I think if you can see it here, then it's ready to be "Closed" and deployed manually:
@@ -16,7 +19,7 @@ val ossrhUsername: String by project
 val ossrhPassword: String by project
 
 plugins {
-    `java-library`
+//    `java-library`
     `maven-publish`
     signing
     id("org.jetbrains.dokka") version "0.10.1"
@@ -24,39 +27,15 @@ plugins {
     kotlin("jvm") version "1.3.71"
 }
 
-//repositories {
-//    mavenLocal()
-////    jcenter()
-//    maven {
-//        url.set("http://repo.maven.apache.org/maven2")
-//    }
-//}
-
 dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit"))
     implementation(kotlin("stdlib-jdk8"))
 }
 
-//task sourcesJar(type: Jar) {
-//    classifier = "sources"
-//    from(sourceSets.main.allSource)
-//}
-//
-//task javadocJar(type: Jar) {
-//    classifier = "javadoc"
-//    from(javadoc.destinationDir)
-//}
-
 group = "org.organicdesign.indented"
-//archivesBaseName = "Indented"
 version = "0.0.11"
 description = "Make debugging methods whose String output compiles to valid Java or Kotlin and is pretty-print indented for easy reading."
-//sourceCompatibility = "1.8"
-
-//artifacts {
-//    archives javadocJar, sourcesJar
-//}
 
 java {
     withJavadocJar()
@@ -75,8 +54,6 @@ publishing {
                 }
             }
             pom {
-//                name.set("Indented")
-//                description.set("Make debugging methods whose String output compiles to valid Java or Kotlin and is pretty-print indented for easy reading.")
                 url.set("https://github.com/GlenKPeterson/Indented")
                 licenses {
                     license {
@@ -136,16 +113,11 @@ tasks {
         outputDirectory = "$buildDir/dokka"
     }
 }
-//dokka {
-//    outputFormat = "html"
-//    outputDirectory = "$buildDir/javadoc"
-//}
 
 signing {
     useGpgCmd()
     sign(publishing.publications["mavenJava"])
 }
-
 
 tasks.compileJava {
     options.encoding = "UTF-8"
